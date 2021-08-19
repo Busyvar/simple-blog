a8f5626b42543d9d96bf73c56b20cd1255bad89d
busyvar <paulemmanuel.lett@posteo.net>
2021-08-19 10:09:01 +0200

    add component and service structure & basic routing & post model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostService } from './post.service';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes : Routes = [
+  { path: 'posts', component: PostListComponent},
+  { path: 'new-post', component: PostFormComponent},
+  { path: "", redirectTo:"posts", pathMatch:"full"},
+  { path: "**", redirectTo:"posts"},
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/post.model.ts
@@ -0,0 +1,9 @@
+export class Post {
+    constructor(
+        private title:string,
+        private content:string,
+        private loveIt:number,
+        private hateIt:number,
+        private date:string,
+        ){}
+}
